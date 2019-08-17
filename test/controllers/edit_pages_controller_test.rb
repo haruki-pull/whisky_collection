6d8a0c21441eb126c718680412cbacaa941a2784
harukipull <harukigit@gmail.com>
2019-08-17 17:42:55 +0900

    ready pages

--- test/controllers/edit_pages_controller_test.rb
+++ test/controllers/edit_pages_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class EditPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get collection_list" do
+    get edit_pages_list_url
+    assert_response :success
+  end
+
+  test "should get collection_individual" do
+    get edit_pages_individual_url
+    assert_response :success
+  end
 end
