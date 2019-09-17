fabeb1c461adc210fc4a2f15f2ccc5630c16be79
harukipull <harukigit@gmail.com>
2019-09-17 16:37:31 +0900

    change submit_tag data_disabled

--- app/controllers/edit_pages_controller.rb
+++ app/controllers/edit_pages_controller.rb
@@ -1,6 +1,7 @@
 class EditPagesController < ApplicationController
 
     def new
+        
     end
       
     def edit_list
@@ -8,6 +9,18 @@ class EditPagesController < ApplicationController
 
     def edit_individual
         @whisky = Whisky.new
+    end
+
+    def create
+        @whisky = Whisky.new(whisky_params)
+        @whisky.save
+        redirect_to edit_individual_path
+    end
+    
+    private
+    
+    def whisky_params
+         params.require(:whisky).permit(:name,kind,comment,rank,picture)
     end
        
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get '/edit_list' ,            to: 'edit_pages#edit_list'
   get '/edit_individual',       to: 'edit_pages#edit_individual'
   get '/new',                   to: 'edit_pages#new'
-  root 'home_pages#home'
+  post '/new',                   to: 'edit_pages#new'
+  root 'edit_pages#edit_individual'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
