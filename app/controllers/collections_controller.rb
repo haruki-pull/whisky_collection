3c89baa284519c195237f3e9a8f4fdc927b2c0a5
harukipull <harukigit@gmail.com>
2019-09-19 14:57:26 +0900

    remake this repository
    このアプリケーションがあまりにもRestfullからかけ離れている
    ことに気づいた。ので同じ機能を持つアプリをいちから作りなおそうと思う

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,7 +1,9 @@
 class CollectionsController < ApplicationController
   def collection_list
+
   end
 
   def collection_individual
+    @whiskies = Whisky.find(params[:whiskies_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
+  resources :collection
+
   get '/collection_list',       to: 'collections#collection_list'
   get '/collection_individual', to: 'collections#collection_individual'
   get '/edit_list' ,            to: 'edit_pages#edit_list'
   get '/edit_individual',       to: 'edit_pages#edit_individual'
   get '/new',                   to: 'edit_pages#new'
+  get '/new',                   to: 'edit_pages#new'
   post '/new',                   to: 'edit_pages#new'
   root 'edit_pages#edit_individual'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
